e6563499130ec760d711e86123522f57bc972286
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-28 14:17:02 +0900

    feat: UserService 에 아이디 중복 체크하는 로직 추가

--- src/main/java/com/example/loginspringboot/service/UserService.java
+++ src/main/java/com/example/loginspringboot/service/UserService.java
@@ -2,11 +2,34 @@ package com.example.loginspringboot.service;
 
 import com.example.loginspringboot.domain.dto.UserDto;
 import com.example.loginspringboot.domain.dto.UserJoinRequest;
+import com.example.loginspringboot.domain.entity.User;
+import com.example.loginspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
     public UserDto join(UserJoinRequest userJoinRequest) {
-        return new UserDto(userJoinRequest.getUserName(), userJoinRequest.getEmailAddress());
+        // 비즈니스 로직 : 회원가입
+
+        // userName(회원 id) 중복 체크, 중복일 시 예외 처리 (사용자 실수는 예외, 런타임 에러는 에러)
+
+        // 회원 가입 .save()
+        return UserDto.builder().build();
+    }
+
+    public UserDto logIn(UserJoinRequest userJoinRequest) {
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent(user -> {throw new RuntimeException("아이디 중복");});
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
+        return UserDto.builder()
+                .userName(savedUser.getUserName())
+                .email(savedUser.getEmailAddress())
+                .phoneNumber(savedUser.getPhoneNumber())
+                .build();
     }
 }
