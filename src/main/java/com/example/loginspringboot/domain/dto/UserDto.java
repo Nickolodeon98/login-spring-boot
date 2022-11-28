df5258758c69943d51c9fa438303b24bab0a65da
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-28 14:15:54 +0900

    feat: UserJoinRequest 와 UserJoinResponse 의 중간 다리 역할을 하는UserDto 에 phoneNumber 변수 추가

--- src/main/java/com/example/loginspringboot/domain/dto/UserDto.java
+++ src/main/java/com/example/loginspringboot/domain/dto/UserDto.java
@@ -1,11 +1,14 @@
 package com.example.loginspringboot.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class UserDto {
     private String userName;
     private String email;
+    private String phoneNumber;
 }
