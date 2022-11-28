e9e19d47b6f92c642c98e3ea7b504563f085be72
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-28 14:16:19 +0900

    feat: UserJoinRequest DTO 를 User 엔티티로 변환하는 메서드 추가

--- src/main/java/com/example/loginspringboot/domain/dto/UserJoinRequest.java
+++ src/main/java/com/example/loginspringboot/domain/dto/UserJoinRequest.java
@@ -1,5 +1,6 @@
 package com.example.loginspringboot.domain.dto;
 
+import com.example.loginspringboot.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,13 @@ public class UserJoinRequest {
     private String password;
     private String emailAddress;
     private String phoneNumber;
+
+    public User toEntity() {
+        return User.builder()
+                .userName(this.userName)
+                .password(this.password)
+                .phoneNumber(this.phoneNumber)
+                .emailAddress(this.emailAddress)
+                .build();
+    }
 }
